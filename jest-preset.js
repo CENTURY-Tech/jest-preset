ab37db95a78ae181e52ad8e37995b4a337ebd4da
Iain Reid <iainreid382@gmail.com>
2019-07-23 18:30:22 +0100

    Add global setup and teardown scripts

--- jest-preset.js
+++ jest-preset.js
@@ -3,10 +3,13 @@
 const path = require("path");
 
 module.exports = {
+  globalSetup: path.resolve(__dirname, "lib", "spec-setup.js"),
+  globalTeardown: path.resolve(__dirname, "lib", "spec-teardown.js"),
+  verbose: true,
   projects: [
     {
       displayName: "Spec",
-      preset: "./node_modules/jest-puppeteer/jest-preset.json",
+      preset: "jest-puppeteer",
       globals: {
         PATH: "http://127.0.0.1:8080"
       },
@@ -18,6 +21,5 @@ module.exports = {
       testRegex: "/src/test/.*\\.unit\\.js$",
       testEnvironment: path.resolve(__dirname, "lib", "unit-environment.js")
     }
-  ],
-  verbose: true
+  ]
 };
